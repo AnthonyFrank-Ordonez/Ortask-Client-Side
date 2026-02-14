11da72efc1599e39fdf89b07c6ecf9cf8951b415
AnthonyFrank-Ordonez <anthonyfrankordonez@yahoo.com>
2026-02-14 13:54:15 +0800

    refactor: Change vite configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 // <reference types="vitest/config" />
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -9,33 +9,37 @@ const ReactCompilerConfig = {
 };
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [
-          ["babel-plugin-react-compiler", ReactCompilerConfig],
-        ]
-      }
-    }),
-    tailwindcss()
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './testSetup.ts'
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3002',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  console.log('🚀 Current Vite Mode:', mode);
+  
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [
+            ["babel-plugin-react-compiler", ReactCompilerConfig],
+          ]
+        }
+      }),
+      tailwindcss()
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },  
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: './testSetup.ts'
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: mode === 'development' ? 'http://localhost:3002' : 'https://ortaskbe.afordonez.com',
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 })
